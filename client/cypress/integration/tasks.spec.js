47d54c634dcfa501e4f094ced9329c82f2f613a9
Andy Bettisworth <andy@accreu.com>
2019-11-27 04:45:57 -0600

    Test for completing tasks

--- client/cypress/integration/tasks.spec.js
+++ client/cypress/integration/tasks.spec.js
@@ -21,4 +21,13 @@ describe("Tasks", () => {
     cy.get('ul').children('li').should('have.length', 2);
     cy.get('ul').contains('Refactor code.');
   })
+
+  it("Completing a task", () => {
+    cy.get('#new-task-form input[name="task"]').type('Completing tasks.');
+    cy.get('#new-task-form button').click();
+    cy.get('#new-task-form input[name="task"]').type('Uncompleting tasks.');
+    cy.get('#new-task-form button').click();
+    cy.get('.li-task').first().click();
+    cy.get('.li-completed').should('have.length', 1);
+  })
 })
