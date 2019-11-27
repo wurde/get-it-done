59189fe627a9c4477dc5d03a18ca2322dd6ba840
Andy Bettisworth <andy@accreu.com>
2019-11-27 04:36:43 -0600

    Check list contains items

--- client/cypress/integration/tasks.spec.js
+++ client/cypress/integration/tasks.spec.js
@@ -11,8 +11,10 @@ describe("Tasks", () => {
     cy.get('#new-task-form input[name="task"]').type('Write tests.');
     cy.get('#new-task-form button').click();
     cy.get('ul').children('li').should('have.length', 1);
+    cy.get('ul').contains('Write tests.');
     cy.get('#new-task-form input[name="task"]').type('Refactor code.');
     cy.get('#new-task-form button').click();
     cy.get('ul').children('li').should('have.length', 2);
+    cy.get('ul').contains('Refactor code.');
   })
 })
